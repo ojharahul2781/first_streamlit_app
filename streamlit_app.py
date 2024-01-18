db534a3fbe647279db81b3aed0634e19a8c39968
ojharahul2781 <156745496+ojharahul2781@users.noreply.github.com>
2024-01-18 11:30:45 +0530

    Update streamlit_app.py
    typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,7 +34,7 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_c
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
 streamlit.dataframe(fruityvice_normalized)
-except URLerror as e:
+except URLError as e:
 streamlit.error()
 # Dont run any command from here while we troubleshout!!!
 
